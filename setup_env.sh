82b3263584f60e7be025008d7c675bbfab3f1cc2
Mario Pavlov <marioppavlov@gmail.com>
2023-11-14 16:08:59 +0200

    feat: Add Oh My Zsh installation and configuration
    This commit adds the functionality to install and configure Oh My Zsh. It uses the official installation script from the Oh My Zsh repository. Additionally, it configures the plugins for Docker and Git in the .zshrc file. If any errors occur during installation or configuration, appropriate error messages are displayed.
    
    It also introduces the Powerlevel10k theme for Oh My Zsh. The commit includes code to clone the Powerlevel10k repository into the custom themes directory of Oh My Zsh and sets it as the default theme in .zshrc.
    
    Furthermore, this commit adds an option to install Vue.js using NVM (Node Version Manager). It installs NVM, asks for a specific version of Node.js to install (latest or 18.10.0), and then asks for a specific version of Vue CLI to install (latest or 5.0.8). Error handling is implemented throughout this process.
    
    Lastly, it introduces a menu-based feature selection loop that allows users to choose additional features like JDK, Vue.js, or Python installations.
    
    Closes #123

--- setup_env.sh
+++ setup_env.sh
@@ -76,10 +76,100 @@ install_zsh() {
     echo "Zsh is now the default shell."
 }
 
+# Install OhMyZsh
 install_oh_my_zsh() {
     echo "Installing Oh My Zsh..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    echo "Oh My Zsh installation complete."
+    if ! sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+        echo "Error: Failed to install Oh My Zsh."
+        return 1
+    fi
+
+    echo "Configuring Oh My Zsh plugins (docker, git)..."
+
+    # Backup the original .zshrc file
+    cp ~/.zshrc ~/.zshrc.backup
+
+    # Add docker and git plugins
+    sed -i 's/^plugins=(/plugins=(docker git /' ~/.zshrc
+
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to configure Oh My Zsh plugins."
+        return 1
+    fi
+
+    echo "Oh My Zsh installation and configuration complete."
+}
+
+# Powerlevel10k theme for ZSH
+install_powerlevel10k() {
+    echo "Installing Powerlevel10k theme for Oh My Zsh..."
+
+    # Clone the Powerlevel10k repository into the Oh My Zsh custom themes directory
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to clone Powerlevel10k repository."
+        return 1
+    fi
+
+    # Set Powerlevel10k as the default theme in .zshrc
+    sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to set Powerlevel10k as the default theme."
+        return 1
+    fi
+
+    echo "Powerlevel10k theme installation complete."
+}
+
+# Install Vue
+install_vue() {
+    echo "Installing NVM (Node Version Manager)..."
+
+    if ! curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash; then
+        echo "Error: Failed to install NVM."
+        return 1
+    fi
+
+    # Load NVM into the current session
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+    # Ask for Node.js version
+    echo "Which version of Node.js would you like to install?"
+    echo "0) Latest"
+    echo "1) 18.10.0"
+    read -p "Enter your choice (1 for 18.10.0, 0 for Latest): " node_choice
+
+    if [ "$node_choice" == "0" ]; then
+        nvm install node || { echo "Error: Failed to install the latest version of Node.js."; return 1; }
+        nvm use node
+    elif [ "$node_choice" == "1" ]; then
+        nvm install 18.10.0 || { echo "Error: Failed to install Node.js version 18.10.0."; return 1; }
+        nvm use 18.10.0
+    else
+        echo "Invalid choice. Installing the latest version of Node.js."
+        nvm install node || { echo "Error: Failed to install the latest version of Node.js."; return 1; }
+        nvm use node
+    fi
+
+    # Ask for Vue CLI version
+    echo "Which version of Vue CLI would you like to install?"
+    echo "0) Latest"
+    echo "1) 5.0.8"
+    read -p "Enter your choice (1 for 5.0.8, 0 for Latest): " vue_cli_choice
+
+    if [ "$vue_cli_choice" == "0" ]; then
+        npm install -g @vue/cli || { echo "Error: Failed to install the latest version of Vue CLI."; return 1; }
+    elif [ "$vue_cli_choice" == "1" ]; then
+        npm install -g @vue/cli@5.0.8 || { echo "Error: Failed to install Vue CLI version 5.0.8."; return 1; }
+    else
+        echo "Invalid choice. Installing the latest version of Vue CLI."
+        npm install -g @vue/cli || { echo "Error: Failed to install the latest version of Vue CLI."; return 1; }
+    fi
+
+    echo "Vue.js environment setup complete."
 }
 
 
@@ -99,8 +189,29 @@ install_git $distro
 
 # Install Zsh
 install_zsh $distro
-
 # Install OhMyZsh
 install_oh_my_zsh
+# Install Powerlevel10k theme
+install_powerlevel10k
+
+while true; do
+    echo "Select the feature to install:"
+    echo "0) Exit"
+    echo "1) JDK"
+    echo "2) Vue.js"
+    echo "3) Python"
+    read -p "Enter your choice [1-4]: " choice
+
+    case $choice in
+        0) break ;;
+        1) install_jdk ;;
+        2) install_vue ;;
+        3) install_python ;;
+        *) echo "Invalid option $choice." ;;
+    esac
+done
+
+
+
 
 # Add additional setup steps here
