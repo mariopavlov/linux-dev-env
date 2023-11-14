beb1d8901761b30e6391848a2aa73904bc4f137b
Mario Pavlov <marioppavlov@gmail.com>
2023-11-14 15:37:30 +0200

    feat: Add setup_env.sh script for Linux Development Environment
    This commit adds a new script, `setup_env.sh`, which automates the process of setting up a Linux Development Environment. The script includes functions to update the system and install Zsh based on the distribution.
    
    Steps:
    - Download this repository on local environment
    - Make the script executable `chmod +x setup_env.sh`
    - Run the script `./setup_env.sh`

---
+++ setup_env.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Global variable for distribution
+distro=""
+
+# Function to update the system
+update_system() {
+    local distro=$1
+    echo "Updating $distro..."
+
+    if [ "$distro" == "ubuntu" ]; then
+        sudo apt update && sudo apt upgrade -y
+    elif [ "$distro" == "fedora" ]; then
+        sudo dnf update -y
+    else
+        echo "Unsupported distribution: $distro"
+        return 1
+    fi
+
+    echo "$distro update complete."
+}
+
+# Function to install Zsh
+install_zsh() {
+    local distro=$1
+    echo "Installing Zsh on $distro..."
+
+    if [ "$distro" == "ubuntu" ]; then
+        sudo apt install zsh -y
+    elif [ "$distro" == "fedora" ]; then
+        sudo dnf install zsh -y
+    else
+        echo "Unsupported distribution for Zsh installation: $distro"
+        return 1
+    fi
+
+    echo "Zsh installation complete."
+}
+
+# Determine the distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    distro=$ID
+else
+    echo "Cannot determine the distribution."
+    exit 1
+fi
+
+# Update the system
+update_system $distro
+
+# Install Zsh
+install_zsh $distro
+
+# Add additional setup steps here
